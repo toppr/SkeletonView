89eb060a70f35dd1da266ddefe494a7874f29bb9
Hamza Ansari <hamzaansari209@gmail.com>
2018-12-17 10:09:16 +0530

    fix color crash

--- Sources/Extensions/UIColor+Skeleton.swift
+++ Sources/Extensions/UIColor+Skeleton.swift
@@ -10,19 +10,37 @@ import UIKit
 
 extension UIColor {
     
-    convenience init(_ hex: UInt) {
-        self.init(
-            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(hex & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
+    //    convenience init(_ hex: UInt) {
+    //        self.init(
+    //            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+    //            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
+    //            blue: CGFloat(hex & 0x0000FF) / 255.0,
+    //            alpha: CGFloat(1.0)
+    //        )
+    //    }
+    
+    convenience init(_ hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt32()
+        Scanner(string: hex).scanHexInt32(&int)
+        let a, r, g, b: UInt32
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
     }
     
     func isLight() -> Bool {
         // algorithm from: http://www.w3.org/WAI/ER/WD-AERT/#color-contrast
         guard let components = cgColor.components,
-              components.count >= 3 else { return false }
+            components.count >= 3 else { return false }
         let brightness = ((components[0] * 299) + (components[1] * 587) + (components[2] * 114)) / 1000
         return !(brightness < 0.5)
     }
@@ -51,25 +69,24 @@ extension UIColor {
 }
 
 public extension UIColor {
-    static var greenSea     = UIColor(0x16a085)
-    static var turquoise    = UIColor(0x1abc9c)
-    static var emerald      = UIColor(0x2ecc71)
-    static var peterRiver   = UIColor(0x3498db)
-    static var amethyst     = UIColor(0x9b59b6)
-    static var wetAsphalt   = UIColor(0x34495e)
-    static var nephritis    = UIColor(0x27ae60)
-    static var belizeHole   = UIColor(0x2980b9)
-    static var wisteria     = UIColor(0x8e44ad)
-    static var midnightBlue = UIColor(0x2c3e50)
-    static var sunFlower    = UIColor(0xf1c40f)
-    static var carrot       = UIColor(0xe67e22)
-    static var alizarin     = UIColor(0xe74c3c)
-    static var clouds       = UIColor(0xecf0f1)
-    static var concrete     = UIColor(0x95a5a6)
-    static var flatOrange   = UIColor(0xf39c12)
-    static var pumpkin      = UIColor(0xd35400)
-    static var pomegranate  = UIColor(0xc0392b)
-    static var silver       = UIColor(0xbdc3c7)
-    static var asbestos     = UIColor(0x7f8c8d)
+    static var greenSea     = UIColor("16a085")
+    static var turquoise    = UIColor("1abc9c")
+    static var emerald      = UIColor("2ecc71")
+    static var peterRiver   = UIColor("3498db")
+    static var amethyst     = UIColor("9b59b6")
+    static var wetAsphalt   = UIColor("34495e")
+    static var nephritis    = UIColor("27ae60")
+    static var belizeHole   = UIColor("2980b9")
+    static var wisteria     = UIColor("8e44ad")
+    static var midnightBlue = UIColor("2c3e50")
+    static var sunFlower    = UIColor("f1c40f")
+    static var carrot       = UIColor("e67e22")
+    static var alizarin     = UIColor("e74c3c")
+    static var clouds       = UIColor("ecf0f1")
+    static var concrete     = UIColor("95a5a6")
+    static var flatOrange   = UIColor("f39c12")
+    static var pumpkin      = UIColor("d35400")
+    static var pomegranate  = UIColor("c0392b")
+    static var silver       = UIColor("bdc3c7")
+    static var asbestos     = UIColor("7f8c8d")
 }
-
